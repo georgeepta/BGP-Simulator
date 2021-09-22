5da830c3b53a4f850691cf2f7f2af16d6652eecd
georgeepta <giwrgosepta1997@hotmail.com>
2021-09-22 17:20:04 +0300

    Some changes in css for the Home page. We add a button that redirects the use to the New Simulation page and we add a gif animation as background.

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-export function Button() {
+export function Button(props) {
+    
+    const className = `btn ${props.type}`
+
     return (
-        <Link to='/sign-up'>
-            <button className='btn'>Sign Up</button>
+        <Link to={props.pagelink}>
+            <button className={className}> {props.btnname} </button>
         </Link>
     );
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import '../App.css';
+import { Button } from '../components/Button';
+import backgroundH from "../images/bg_graph.gif";
+
 
 export default function Home() {
   return (
-    <>
-      <h1 className='home'>HOMEPAGE</h1>
-    </>
+    <div className="bk_Img"
+      style={{
+        backgroundImage: "url(" + backgroundH + ")",
+        backgroundSize: "cover",
+        height: "100vh",
+      }}
+    >
+      <div className='home'>
+        <div className='home-launch'>
+          <h1 className='h1'>Launch a New Simulation now!</h1>
+          <div className="line-break"></div>
+          <Button 
+            type="success"
+            pagelink="./new-simulation" 
+            btnname="Start" 
+          />
+        </div>
+      </div>
+    </div>
   );
 }
