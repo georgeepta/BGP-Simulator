998fac38380c711885d212f20bad44e5e530ef78
georgeepta <giwrgosepta1997@hotmail.com>
2021-09-25 04:28:43 +0300

    Request Validation has been added for the POST data of the custom simulation.

--- backend/api/SimulationRequestHandler.py
+++ backend/api/SimulationRequestHandler.py
@@ -1,8 +1,10 @@
 import json
 import random
 import psycopg2
+import ipaddress
 from copy import deepcopy
 from datetime import datetime, timezone
+from flask import Response
 from flask_restful import Resource, reqparse
 from backend.api.SimulationConstructor import SimulationConstructor
 from mpipe import Pipeline, Stage
@@ -51,92 +53,154 @@ class SimulationRequestHandler(Resource):
         print('Loading topology...')
         Topo.load_topology_from_csv(
             '../datasets/CAIDA AS-graph/serial-2/' + sim_data['caida_as_graph_dataset'] + '.as-rel2.txt')
+        '''
+        We comment the following lines, because are useful only if the members of the IXPs 
+        that are not exists in the Topology are going to be insert in it.
+        s 
         Topo.load_ixps_from_json('../datasets/CAIDA IXPS/' + 'ixs_' + sim_data['caida_ixps_datasets'] + '.jsonl',
                                  '../datasets/CAIDA IXPS/' + 'ix-asns_' + sim_data['caida_ixps_datasets'] + '.jsonl')
         Topo.add_extra_p2p_custom_links()
+        '''
 
+    def validate_ASN(self, ASN_List, ASN):
+        if ASN in ASN_List:
+            return ""
+        else:
+            return "ASN {} is not valid or unavailable for simulation.".format(ASN)
+
+    def validate_IP_Address(self, prefix):
+        try:
+            ipaddress.ip_network(prefix)
+            return ""
+        except ValueError:
+            return "IP prefix {} is not valid.".format(prefix)
+
+    def validate_request_data(self, ASN_List, sim_data):
+        isValidData = True
+        errors_dict = {}
+        if sim_data['simulation_type'] not in ["custom", "random"]:
+            errors_dict['simulation_type'] = "Invalid simulation type --> type\"custom\" or \"random\""
+        else:
+            errors_dict['simulation_type'] = ""
+            errors_dict['hijack_prefix_type'] = "" if sim_data['hijack_prefix_type'] in ["exact", "subprefix"] else "Invalid hijack attack --> type \"exact\" or \"subprefix\""
+            errors_dict['rpki_rov_mode'] = "" if sim_data['rpki_rov_mode'] in ["all", "random_20"] else "Invalid RPKI ROV MODE --> type \"all\" or \"random_20\""
+            #It is not required to validate the following inputs:
+            #hijack_type, caida_as_graph_dataset, caida_ixps_datasets, realistic_rpki_rov
+            #nb_of_sims, nb_of_reps, max_nb_anycast_ASes
+
+            if sim_data['simulation_type'] == "custom":
+                errors_dict['legitimate_AS'] = self.validate_ASN(ASN_List, sim_data['legitimate_AS'])
+                errors_dict['hijacker_AS'] = self.validate_ASN(ASN_List, sim_data['hijacker_AS'])
+                anycast_err_str = ""
+                for asn in sim_data['anycast_ASes']:
+                    if asn not in ASN_List:
+                        anycast_err_str += str(asn) + ","
+                errors_dict['anycast_ASes'] = "" if anycast_err_str == "" else "ASN " + anycast_err_str + " not valid or unavailable for simulation."
+                errors_dict['legitimate_prefix'] = self.validate_IP_Address(sim_data['legitimate_prefix'])
+                errors_dict['hijacker_prefix'] = self.validate_IP_Address(sim_data['hijacker_prefix'])
+                errors_dict['mitigation_prefix'] = self.validate_IP_Address(sim_data['mitigation_prefix'])
+
+        for item in errors_dict:
+            if errors_dict[item] != "":
+                print("Invalid simulation data in the POST request!")
+                isValidData = False
+                break
+
+        return isValidData, errors_dict
 
 
     def post(self):
         req_parser = reqparse.RequestParser()
-        req_parser.add_argument('simulation_type', type=str, help="Simulation type is required (custom or as-vulnerability or country-vulnerability)")
+        req_parser.add_argument('simulation_type', type=str, required=True, help="Simulation type is required (custom or as-vulnerability or country-vulnerability)")
         req_parser.add_argument('legitimate_AS', type=int, help="ASN of victim is required (e.g., 12345)")
         req_parser.add_argument('legitimate_prefix', type=str, help="CIDR prefix of victim is required (e.g., 1.2.3.0/24)")
         req_parser.add_argument('hijacker_AS', type=int, help="ASN of hijacker is required (e.g., 67890)")
         req_parser.add_argument('hijacker_prefix', type=str, help="CIDR prefix of hijacker is required (e.g., 1.2.3.0/24)")
-        req_parser.add_argument('hijack_type', type=int, help="Must be an integer in {0,1,2,3,...} denoting the type of hijacking attack, with 0 = origin AS attack , 1 = 1st hop attack, etc.")
-        req_parser.add_argument('hijack_prefix_type', type=str, help="Must be a string in {exact, subprefix} denoting exact or subprefix announcement")
+        req_parser.add_argument('hijack_type', type=int, required=True, help="Must be an integer in {0,1,2,3,...} denoting the type of hijacking attack, with 0 = origin AS attack , 1 = 1st hop attack, etc.")
+        req_parser.add_argument('hijack_prefix_type', type=str, required=True, help="Must be a string in {exact, subprefix} denoting exact or subprefix announcement")
         req_parser.add_argument('anycast_ASes', type=list, location='json', help="Must be a list of integers denoting the ASNs of the helper ASes (e.g., [12345, 67890, ...])")
         req_parser.add_argument('mitigation_prefix', type=str, help="CIDR mitigation prefix that is going to announced by helper AS and victim AS (e.g., 1.2.3.0/25)")
-        req_parser.add_argument('realistic_rpki_rov', type=bool, help="A boolean variable denoting if the simulation should use the most recent from the RIR databases with the help of the Routinator or just to make theoretical assumptions for the RPKI ROV")
-        req_parser.add_argument('rpki_rov_mode', type=str, help="Must be a string denoting the RPKI Route Origin Validation mode (e.g., disabled, all, 20%, ...")
-        req_parser.add_argument('nb_of_sims', type=int, help="An integer denoting the number of simulations")
-        req_parser.add_argument('nb_of_reps', type=int, help="An integer denoting the number of experiment runs (repetitions) of each simulation")
+        req_parser.add_argument('realistic_rpki_rov', type=bool, required=True, help="A boolean variable denoting if the simulation should use the most recent from the RIR databases with the help of the Routinator or just to make theoretical assumptions for the RPKI ROV")
+        req_parser.add_argument('rpki_rov_mode', type=str, required=True, help="Must be a string denoting the RPKI Route Origin Validation mode (e.g., disabled, all, 20%, ...")
+        req_parser.add_argument('nb_of_sims', type=int, required=True, help="An integer denoting the number of simulations")
+        req_parser.add_argument('nb_of_reps', type=int, required=True, help="An integer denoting the number of experiment runs (repetitions) of each simulation")
         req_parser.add_argument('caida_as_graph_dataset', type=str, help="A string of type yyyymmdd denoting the CAIDA AS-graph dataset from which the topology will be loaded")
         req_parser.add_argument('caida_ixps_datasets', type=str, help="A string of type yyyymm denoting the CAIDA IXPs datasets (ix-asns_yyyymm.jsonl and ixs_yyyymm.jsonl) from which the topology generate the links between AS-IXPS")
-        req_parser.add_argument('max_nb_anycast_ASes', type=int, help="An integer denoting the maximum number of anycast ASes to be used for hijack mitigation")
+        req_parser.add_argument('max_nb_anycast_ASes', type=int, required=True, help="An integer denoting the maximum number of anycast ASes to be used for hijack mitigation")
 
         sim_data = req_parser.parse_args()
 
         '''
-        create a connection to the database
+        Initiaze a Topology and get the list of all ASNs: 
+        useful for the validation and random simulations  
         '''
-        conn = self.connect_to_db("bgp_simulator", 'gepta', '1821', '127.0.0.1', '5432')
+        Topo = BGPtopology()
+        self.load_create_Topology(Topo, sim_data)
+        ASN_List = Topo.get_all_nodes_ASNs()
+        del Topo
 
         '''
-        insert simulation data in database
+        Validate POST request data
         '''
-        simulation_uuid = self.insert_simulation_data_in_db(sim_data, conn)
+        isValidData, errors_dict = self.validate_request_data(ASN_List, sim_data)
 
-        '''
-        close connection to database
-        '''
-        conn.close()
+        if isValidData:
 
-        '''
-        Instantiate the BGP Hijacking Simulations
-        '''
+            '''
+            create a connection to the database
+            '''
+            conn = self.connect_to_db("bgp_simulator", 'gepta', '1821', '127.0.0.1', '5432')
 
-        Stage1 = Stage(worker_class=SimulationConstructor, size=2, do_stop_task=True, disable_result=False)
-        pipe = Pipeline(Stage1)
+            '''
+            insert simulation data in database
+            '''
+            simulation_uuid = self.insert_simulation_data_in_db(sim_data, conn)
 
-        print('Simulation started')
-        if sim_data['simulation_type'] == "custom":
-            for task in range(0, sim_data['nb_of_sims']):
-                task_data = {"simulation_uuid": simulation_uuid, "sim_data": sim_data}
-                pipe.put(task_data)
-            ## Term signal
-            pipe.put(None)
+            '''
+            close connection to database
+            '''
+            conn.close()
+
+            '''
+            Instantiate the BGP Hijacking Simulations
+            '''
+
+            Stage1 = Stage(worker_class=SimulationConstructor, size=2, do_stop_task=True, disable_result=False)
+            pipe = Pipeline(Stage1)
+
+            print('Simulation started')
+            if sim_data['simulation_type'] == "custom":
+                for task in range(0, sim_data['nb_of_sims']):
+                    task_data = {"simulation_uuid": simulation_uuid, "sim_data": sim_data}
+                    pipe.put(task_data)
+                ## Term signal
+                pipe.put(None)
+
+            else:
+                for task in range(0, sim_data['nb_of_sims']):
+                    '''
+                    Full randomness at each simulation
+                    '''
+                    random_ASNs = random.sample(ASN_List, 2 + sim_data['max_nb_anycast_ASes'])
+                    sim_data['legitimate_AS'] = random_ASNs[0]
+                    sim_data['hijacker_AS'] = random_ASNs[1]
+                    sim_data['anycast_ASes'] = random_ASNs[2:]
+                    '''
+                    Alternative selection (victim, hijacker of this simulation are not available on the other simulations)
+                    
+                    sim_data['legitimate_AS'] = ASN_List.pop(random.randrange(len(ASN_List)))
+                    sim_data['hijacker_AS'] = ASN_List.pop(random.randrange(len(ASN_List)))
+                    sim_data['anycast_ASes'] = random.sample(ASN_List, sim_data['max_nb_anycast_ASes'])                
+                    '''
+                    task_data = {"simulation_uuid": simulation_uuid, "sim_data": deepcopy(sim_data)}
+                    pipe.put(task_data)
+
+                ## Term signal
+                pipe.put(None)
+
+
+            return {'status': "Simulation start!"}, 200
 
         else:
-            # Random simulation --> get the list of all ASN
-            Topo = BGPtopology()
-            self.load_create_Topology(Topo, sim_data)
-            ASN_List = Topo.get_all_nodes_ASNs()
-            del Topo
-
-            for task in range(0, sim_data['nb_of_sims']):
-                '''
-                Full randomness at each simulation
-                '''
-                random_ASNs = random.sample(ASN_List, 2 + sim_data['max_nb_anycast_ASes'])
-                sim_data['legitimate_AS'] = random_ASNs[0]
-                sim_data['hijacker_AS'] = random_ASNs[1]
-                sim_data['anycast_ASes'] = random_ASNs[2:]
-                '''
-                Alternative selection (victim, hijacker of this simulation are not available on the other simulations)
-                
-                sim_data['legitimate_AS'] = ASN_List.pop(random.randrange(len(ASN_List)))
-                sim_data['hijacker_AS'] = ASN_List.pop(random.randrange(len(ASN_List)))
-                sim_data['anycast_ASes'] = random.sample(ASN_List, sim_data['max_nb_anycast_ASes'])                
-                '''
-                task_data = {"simulation_uuid": simulation_uuid, "sim_data": deepcopy(sim_data)}
-                pipe.put(task_data)
-
-            ## Term signal
-            pipe.put(None)
-
-
-        return {
-            'simulation_type': sim_data
-        }
+            response = Response(response=json.dumps(errors_dict), status=400, mimetype='application/json')
+            return response
--- frontend/src/pages/CustomSimulation.js
+++ frontend/src/pages/CustomSimulation.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import '../App.css';
 import './CustomSimulation.css';
 
@@ -16,6 +17,24 @@ function CustomSimulation() {
     const [RealisticROV, setRealisticROV] = useState(true);
     const [RovMode, setRovMode] = useState('all');
     const [NumOfSimReps, setNumOfSimReps] = useState(1);
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const [errorMessage, setMessage] = useState(false);
+    const [errorText, setErrorText] = useState('');
+    const [errorMessageVictimASN, setMessageVictimASN] = useState(false);
+    const [errorTextVictimASN, setErrorTextVictimASN] = useState('');
+    const [errorMessageVictimPrefix, setMessageVictimPrefix] = useState(false);
+    const [errorTextVictimPrefix, setErrorTextVictimPrefix] = useState('');
+    const [errorMessageHijackerASN, setMessageHijackerASN] = useState(false);
+    const [errorTextHijackerASN, setErrorTextHijackerASN] = useState('');
+    const [errorMessageHijackerPrefix, setMessageHijackerPrefix] = useState(false);
+    const [errorTextHijackerPrefix, setErrorTextHijackerPrefix] = useState('');
+    const [errorMessageAnycastASes, setMessageAnycastASes] = useState(false);
+    const [errorTextAnycastASes, setErrorTextAnycastASes] = useState('');
+    const [errorMessageMitigationPrefix, setMessageMitigationPrefix] = useState(false);
+    const [errorTextMitigationPrefix, setErrorTextMitigationPrefix] = useState('');
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -23,13 +42,13 @@ function CustomSimulation() {
         const sim_data = {
             "simulation_type": "custom",
             "legitimate_AS": parseInt(VictimASN),
-            "legitimate_prefix": VictimPrefix,
+            "legitimate_prefix": VictimPrefix.replace(/\s+/g,""),
             "hijacker_AS": parseInt(HijackerASN),
-            "hijacker_prefix": HijackerPrefix,
+            "hijacker_prefix": HijackerPrefix.replace(/\s+/g,""),
             "hijack_type": HijackType,
             "hijack_prefix_type": HijackPrefixType,
             "anycast_ASes": anycast_ASes_list,
-            "mitigation_prefix": MitigationPrefix,
+            "mitigation_prefix": MitigationPrefix.replace(/\s+/g,""),
             "realistic_rpki_rov": RealisticROV,
             "rpki_rov_mode": RovMode,
             "nb_of_sims": NumOfSimReps,
@@ -38,6 +57,8 @@ function CustomSimulation() {
             "caida_ixps_datasets": "202104",
             "max_nb_anycast_ASes": anycast_ASes_list.length
         };
+
+        setIsPending(true);
 
         fetch('http://127.0.0.1:5000/launch_simulation', {
             method: 'POST',
@@ -50,15 +71,75 @@ function CustomSimulation() {
             // check for error response
             if (!response.ok) {
                 // get error message from body or default to response status
-                const error = (data && data.message) || response.status;
-                return Promise.reject(error);
+                console.log(data)
+                const error = data || response.status;
+                throw error;
+                /*return Promise.reject(error);*/
             }
 
             //Successful Request --> do some action
             console.log(data)
+            setIsPending(false);
+            history.push('/simulation-events');
 
         }).catch(error => {
-            console.error('There was an error!', error);
+            console.error('There was an error!', error.message);
+            
+            
+            if (error.message){
+                setMessage(true);
+                setErrorText(error.message);
+            }else{
+                setMessage(false);
+                setErrorText("");
+            }
+
+            if (error.legitimate_AS !== ""){
+                setMessageVictimASN(true);
+                setErrorTextVictimASN(error.legitimate_AS);
+            }else{
+                setMessageVictimASN(false);
+                setErrorTextVictimASN("");
+            }
+            if (error.legitimate_prefix !== ""){
+                setMessageVictimPrefix(true);
+                setErrorTextVictimPrefix(error.legitimate_prefix);
+            }else{
+                setMessageVictimPrefix(false);
+                setErrorTextVictimPrefix("");
+            }
+
+            if (error.hijacker_AS !== ""){
+                setMessageHijackerASN(true);
+                setErrorTextHijackerASN(error.hijacker_AS);
+            }else{
+                setMessageHijackerASN(false);
+                setErrorTextHijackerASN("");
+            }
+            if (error.hijacker_prefix !== ""){
+                setMessageHijackerPrefix(true);
+                setErrorTextHijackerPrefix(error.hijacker_prefix);
+            }else{
+                setMessageHijackerPrefix(false);
+                setErrorTextHijackerPrefix("");
+            }
+
+            if (error.anycast_ASes !== ""){
+                setMessageAnycastASes(true);
+                setErrorTextAnycastASes(error.anycast_ASes);
+            }else{
+                setMessageAnycastASes(false);
+                setErrorTextAnycastASes("");
+            }
+            if (error.mitigation_prefix !== ""){
+                setMessageMitigationPrefix(true);
+                setErrorTextMitigationPrefix(error.mitigation_prefix);
+            }else{
+                setMessageMitigationPrefix(false);
+                setErrorTextMitigationPrefix("");
+            }
+
+            setIsPending(false);
         });
 
     }
@@ -76,16 +157,22 @@ function CustomSimulation() {
                 <label>Hijack Type:</label>
                 <input type="number" min="0" value={HijackType} onChange={(e) => setHijackType(e.target.value)} required />
                 <label>Victim's ASN:</label>
+                {errorMessageVictimASN && <p style={{color: "red", fontSize: "small"}}>{errorTextVictimASN}</p>}
                 <input type="number" min="0" value={VictimASN} onChange={(e) => setVictimASN(e.target.value)} required />
                 <label>Victim's Prefix:</label>
+                {errorMessageVictimPrefix && <p style={{color: "red", fontSize: "small"}}>{errorTextVictimPrefix}</p>}
                 <input type="text" value={VictimPrefix} onChange={(e) => setVictimPrefix(e.target.value)} required />
                 <label>Hijacker's ASN:</label>
+                {errorMessageHijackerASN && <p style={{color: "red", fontSize: "small"}}>{errorTextHijackerASN}</p>}
                 <input type="number" min="0" value={HijackerASN} onChange={(e) => setHijackerASN(e.target.value)} required />
                 <label>Hijacker's Prefix:</label>
+                {errorMessageHijackerPrefix && <p style={{color: "red", fontSize: "small"}}>{errorTextHijackerPrefix}</p>}
                 <input type="text" value={HijackerPrefix} onChange={(e) => setHijackerPrefix(e.target.value)} required />
                 <label>Anycast ASes:</label>
+                {errorMessageAnycastASes && <p style={{color: "red", fontSize: "small"}}>{errorTextAnycastASes}</p>}
                 <input type="text" value={AnycastASes} onChange={(e) => setAnycastASes(e.target.value)} required />
                 <label>Mitigation Prefix:</label>
+                {errorMessageMitigationPrefix && <p style={{color: "red", fontSize: "small"}}>{errorTextMitigationPrefix}</p>}
                 <input type="text" value={MitigationPrefix} onChange={(e) => setMitigationPrefix(e.target.value)} required />
                 <label>Realistic RPKI ROV:</label>
                 <select value={RealisticROV} onChange={(e) => setRealisticROV(e.target.value)}>
@@ -98,8 +185,10 @@ function CustomSimulation() {
                     <option value="random_20">Random 20%</option>
                 </select>
                 <label>Number of Simulation Repetitions:</label>
-                <input type="number" min="1" value={NumOfSimReps} onChange={(e) => setNumOfSimReps(e.target.value)} required />
-                <button>Launch Simulation</button>
+                <input type="number" min="1" max="100" value={NumOfSimReps} onChange={(e) => setNumOfSimReps(e.target.value)} required />
+                {errorMessage && <p style={{color: "red", fontSize: "small"}}>{errorText}</p>}
+                {!isPending && <button>Launch Simulation</button>}
+                {isPending && <button disabled>Send Simulation Data...</button>}
             </form>
         </div>
     );
