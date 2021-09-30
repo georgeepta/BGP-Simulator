a1f0ffafe8b1a35e9ad4e9fac5845109a2fbbc37
georgeepta <giwrgosepta1997@hotmail.com>
2021-09-30 03:30:13 +0300

    Some testing in css in order to decide the overview section of the Simulation Details page.

--- frontend/src/pages/SimulationDetails.js
+++ frontend/src/pages/SimulationDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router';
 import '../App.css';
+import './SimulationDetails.css';
 
 function SimulationDetails() {
     
@@ -11,6 +12,40 @@ function SimulationDetails() {
         <div className='simulation-details'>
             <h1>Simulation Details</h1>
             <p>{simulation_uuid}</p>
+            <div className='row'>
+                <table className='table-striped'>
+                    <tbody>
+                        <tr>
+                            <th>Simulation ID:</th>
+                            <td>{simulation_uuid}</td>
+                        </tr>
+                        <tr>
+                            <th>Simulation ID:</th>
+                            <td>{simulation_uuid}</td>
+                        </tr>
+                        <tr>
+                            <th>Simulation ID:</th>
+                            <td>{simulation_uuid}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className='table-striped'>
+                    <tbody>
+                        <tr>
+                            <th>Simulation ID:</th>
+                            <td>{simulation_uuid}</td>
+                        </tr>
+                        <tr>
+                            <th>Simulation ID:</th>
+                            <td>{simulation_uuid}</td>
+                        </tr>
+                        <tr>
+                            <th>Simulation ID:</th>
+                            <td>{simulation_uuid}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
   );
 }
